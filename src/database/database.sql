4fdae10ff0eae0736af2f869b9bed8fc635fbde0
Rafael-Conrado <retamozor@uninorte.edu.com>
2021-05-29 19:10:57 -0500

    base de datos

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,2 +1,27 @@
 CREATE DATABASE casoscovid;
 \c casoscovid
+
+CREATE TABLE userdata (
+  id SERIAL NOT NULL PRIMARY KEY,
+  nombre VARCHAR(40) NOT NULL,
+  apellido VARCHAR(40) NOT NULL,
+  rol VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE passwords (
+  username VARCHAR(50) NOT NULL PRIMARY KEY,
+  pass VARCHAR(255) NOT NULL,
+  user_id INT REFERENCES userdata (id) NOT NULL
+);
+
+CREATE TABLE examen (
+  id SERIAL NOT NULL PRIMARY KEY,
+  nombre VARCHAR(40) NOT NULL,
+  cedula INT NOT NULL,
+  sexo VARCHAR(1) NOT NULL,
+  nacimiento DATE NOT NULL,
+  residencia VARCHAR(100) NOT NULL,
+  trabajo VARCHAR(100),
+  examen VARCHAR(1) NOT NULL,
+  fecha TIMESTAMP NOT NULL
+);
